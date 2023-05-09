b513c0ccc6b258a86bc843eb5ec18722484696ac
Ho Jie Feng <hjf3108@gmail.com>
2023-05-09 21:37:30 +0800

    Implement download of discussions and announcements

--- src/main.rs
+++ src/main.rs
@@ -430,6 +430,22 @@ async fn process_data(
         (String, PathBuf),
         options.clone()
     );
+    let discussions_path = path.join("discussions");
+    create_folder_if_not_exist(&discussions_path)?;
+    fork!(
+        process_discussions,
+        (url.clone(), false, discussions_path),
+        (String, bool, PathBuf),
+        options.clone()
+    );
+    let announcements_path = path.join("announcements");
+    create_folder_if_not_exist(&announcements_path)?;
+    fork!(
+        process_discussions,
+        (url.clone(), true, announcements_path),
+        (String, bool, PathBuf),
+        options.clone()
+    );
     Ok(())
 }
 
@@ -451,6 +467,116 @@ async fn process_users (
             .write_all(page_body.as_bytes())
             .with_context(|| format!("Unable to write to file for {:?}", users_path))?;
     }
+
+    Ok(())
+}
+
+async fn process_discussions(
+    (url, announcement, path): (String, bool, PathBuf),
+    options: Arc<ProcessOptions>,
+) -> Result<()> {
+    let discussion_url = format!("{}discussion_topics{}", url, if announcement { "?only_announcements=true" } else { "" });
+    let pages = get_pages(discussion_url, &options).await?;
+
+    let discussion_path = path.join("discussions.json");
+    let mut discussion_file = std::fs::File::create(discussion_path.clone())
+        .with_context(|| format!("Unable to create file for {:?}", discussion_path))?;
+
+    for pg in pages {
+        let uri = pg.url().to_string();
+        let page_body = pg.text().await?;
+
+        discussion_file
+            .write_all(page_body.as_bytes())
+            .with_context(|| format!("Unable to write to file for {:?}", discussion_path))?;
+
+        let discussion_result = serde_json::from_str::<canvas::DiscussionResult>(&page_body);
+
+        match discussion_result {
+            Ok(canvas::DiscussionResult::Ok(discussions)) => {
+                for discussion in discussions {
+                    // download attachments
+                    let discussion_path = path.join(format!("{}_{}", discussion.id, sanitize_filename::sanitize(discussion.title)));
+                    create_folder_if_not_exist(&discussion_path)?;
+
+                    let files = discussion.attachments
+                        .into_iter()
+                        .map(|mut f| {
+                            f.display_name = format!("{}_{}", f.id, &f.display_name);
+                            f
+                        })
+                        .collect();
+                    {
+                        let mut filtered_files = filter_files(&options, &path, files);
+                        let mut lock = options.files_to_download.lock().await;
+                        lock.append(&mut filtered_files);
+                    }
+                    
+                    let view_url = format!("{}discussion_topics/{}/view", url, discussion.id);
+                    fork!(
+                        process_discussion_view,
+                        (view_url, discussion_path),
+                        (String, PathBuf),
+                        options.clone()
+                    )
+                }
+            }
+            Ok(canvas::DiscussionResult::Err { status }) => {
+                eprintln!(
+                    "Failed to access discussions at link:{uri}, path:{path:?}, status:{status}",
+                );
+            }
+            Err(e) => {
+                eprintln!("Error when getting discussions at link:{uri}, path:{path:?}\n{e:?}",);
+            }
+        }
+    }
+    Ok(())
+}
+
+async fn process_discussion_view(
+    (url, path): (String, PathBuf),
+    options: Arc<ProcessOptions>,
+) -> Result<()> {
+    let resp = get_canvas_api(url.clone(), &options).await?;
+    let discussion_view_body = resp.text().await?;
+    
+    let discussion_view_json = path.join("discussion.json");
+    let mut discussion_view_file = std::fs::File::create(discussion_view_json.clone())
+        .with_context(|| format!("Unable to create file for {:?}", discussion_view_json))?;
+
+    discussion_view_file
+        .write_all(discussion_view_body.as_bytes())
+        .with_context(|| format!("Unable to write to file for {:?}", discussion_view_json))?;
+
+    let discussion_view_result = serde_json::from_str::<canvas::DiscussionView>(&discussion_view_body);
+    let mut attachments_all = Vec::new();
+    match discussion_view_result {
+        Result::Ok(discussion_view) => {
+            for view in discussion_view.view {
+                if let Some(mut attachments) = view.attachments {
+                    attachments_all.append(&mut attachments);
+                };
+                if let Some(attachment) = view.attachment {
+                    attachments_all.push(attachment);
+                };
+            }
+        }
+        Result::Err(e) => {
+            eprintln!("Error when getting submissions at link:{url}, path:{path:?}\n{e:?}",);
+        }
+    }
+
+    let files = attachments_all
+        .into_iter()
+        .map(|mut f| {
+            f.display_name = format!("{}_{}", f.id, &f.display_name);
+            f
+        })
+        .collect();
+    let mut filtered_files = filter_files(&options, &path, files);
+    let mut lock = options.files_to_download.lock().await;
+    lock.append(&mut filtered_files);
 
     Ok(())
 }
@@ -650,6 +776,34 @@ mod canvas {
     pub(crate) enum FileResult {
         Err { status: String },
         Ok(Vec<File>),
+    }
+
+    #[derive(Deserialize)]
+    #[serde(untagged)]
+    pub(crate) enum DiscussionResult {
+        Err { status: String },
+        Ok(Vec<Discussion>),
+    }
+    #[derive(Clone, Debug, Deserialize)]
+    pub struct Discussion {
+        pub id: u32,
+        pub title: String,
+        pub message: String,
+        pub attachments: Vec<File>,
+    }
+
+    #[derive(Clone, Debug, Deserialize)]
+    pub struct DiscussionView {
+        pub unread_entries: Vec<u32>,
+        pub view: Vec<Comments>,
+    }
+
+    #[derive(Clone, Debug, Deserialize)]
+    pub struct Comments {
+        pub id: u32,
+        pub message: Option<String>,
+        pub attachment: Option<File>,
+        pub attachments: Option<Vec<File>>,
     }
 
     #[derive(Clone, Debug, Deserialize)]
